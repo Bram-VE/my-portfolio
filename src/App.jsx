7fdc2e7c7028bcb82f0ad1151be0732164417d64
Bram-VE <vaneccelpoelbram3@gmail.com>
2025-04-23 14:41:58 +0200

    Final push

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function Main() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
