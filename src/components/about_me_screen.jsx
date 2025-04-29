157b4de0572dbc4c290c57cbe713469a96400090
Bram-VE <vaneccelpoelbram3@gmail.com>
2025-04-29 09:57:12 +0200

    Updated CV Download

--- src/components/about_me_screen.jsx
+++ src/components/about_me_screen.jsx
@@ -14,7 +14,6 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faMicrosoft, faJava, faPython } from '@fortawesome/free-brands-svg-icons';
 import profilePicture from '@/assets/profile_picture.jpg';
-import cv from '@/assets/CV_Bram Van Eccelpoel.pdf'; 
 
 function About() {
   return (
@@ -28,7 +27,9 @@ function About() {
             <p>
               I'm a passionate student currently studying <strong>Application Development</strong>. I chose this course because I wanted to learn more about technology and how to build apps. I love to code, build apps, and experiment with game development in my free time.
             </p>
-            <a href={cv} download="CV_Bram Van Eccelpoel.pdf" className={styles.downloadButton}>Download CV</a>
+            <a href="src/assets/CV_Bram Van Eccelpoel.pdf" download="CV_Bram Van Eccelpoel.pdf" className={styles.downloadButton}>
+              Download CV
+            </a>
           </div>
         </section>
 
